e1e6eaf457087719991665bce148dfc5187aaef7
ferhat945 <ferhatmenekse945@gmail.com>
2026-04-16 17:33:19 +0300

    fix sitemap typing

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,20 @@
 import { MetadataRoute } from "next"
 import { prisma } from "@/lib/prisma"
 
+type SitemapBlogItem = {
+  slug: string
+  updatedAt: Date
+}
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://adanaestetik.com"
 
-  const blogs = await prisma.blog.findMany({
+  const blogs: SitemapBlogItem[] = await prisma.blog.findMany({
     where: { isPublished: true },
     select: { slug: true, updatedAt: true },
   })
 
-  const blogUrls = blogs.map((blog) => ({
+  const blogUrls = blogs.map((blog: SitemapBlogItem) => ({
     url: `${baseUrl}/blog/${blog.slug}`,
     lastModified: blog.updatedAt,
   }))
@@ -21,6 +26,22 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
     {
       url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/hakkimizda`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/iletisim`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ucretsiz-danisma`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/kvkk`,
       lastModified: new Date(),
     },
     ...blogUrls,
