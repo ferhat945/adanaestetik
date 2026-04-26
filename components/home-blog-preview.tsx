74fe6234b00a446fb2d1478bb0c6483b776f3a4d
ferhat945 <ferhatmenekse945@gmail.com>
2026-04-26 18:53:41 +0300

    fix blog cache

--- components/home-blog-preview.tsx
+++ components/home-blog-preview.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import { prisma } from "@/lib/prisma"
 
+export const dynamic = "force-dynamic" // 🔥 EKLENDİ
+
 type HomeBlogPreviewItem = {
   id: number
   title: string
